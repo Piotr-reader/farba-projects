992e449513eed1bd7e927826553de3e5bd8b2ab3
Piotr-reader <piotr_partsianko@inbox.ru>
2023-04-08 22:55:14 +0300

    feat: implement toggle function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const accordion = document.querySelector(".accordion");
+accordion.addEventListener("click", (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+function toggleAccordion(panelToActive) {
+  const buttons = panelToActive.parentElement.querySelectorAll("button");
+  const contents = panelToActive.parentElement.querySelectorAll(".accordion-content");
+  buttons.forEach((button) => {
+    button.setAttribute("aria-expanded", false);
+  });
+  contents.forEach((content) => {
+    content.setAttribute("aria-hidden", true);
+  });
+  panelToActive.querySelector("button").setAttribute("aria-expanded", true);
+  panelToActive.querySelector(".accordion-content").setAttribute("aria-hidden", false);
+}
